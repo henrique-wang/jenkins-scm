516042e835dd4c68b46d7f3b61abc6f1ac0da9f5
Henrique <henrique.wang44@gmail.com>
2023-05-26 16:55:02 -0300

    Force unit test error

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,7 +1,7 @@
 const operators = require('../src/utils/operators');
 
 test('it should return 3 when operands are 1 and 2', () => {
-    expect(operators.sum(1, 2)).toEqual(3);
+    expect(operators.sum(1, 2)).toEqual(4);
 });
 
 test('it should throw an error when an operand is not a number', () => {
